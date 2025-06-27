4de28c5a99d2afbdf4228ed393f04170818678cd
Rahul Raj <rahul.raj9237@gmail.com>
2025-06-27 21:36:44 +0530

    checkpoints -langgraph

---
+++ 09_ChatGraph/graph.py
@@ -0,0 +1,47 @@
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START,END
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class State(TypedDict):
+    messages: Annotated[list,add_messages]
+    
+
+llm=init_chat_model(model_provider='openai', model='gpt-4.1')
+
+def chat_model(state:State):
+    response =state["messages"]
+    return {"messages":[response]}
+
+graph_builder=StateGraph(State)
+
+graph_builder.add_node("chat_model",chat_model)
+graph_builder.add_edge(START,"chat_model")
+graph_builder.add_edge("chat_model",END)
+
+def compile_graph_with_checkpointer(checkpointer):
+    graph_with_checkpointer = graph_builder.compile(checkpointer=checkpointer)
+    return graph_with_checkpointer
+
+def main():
+    MONGO_URI= os.getenv("MONGO_URI")
+    config = {"configurable": {"thread_id": "1"}}
+    with MongoDBSaver.from_conn_string(MONGO_URI) as mongo_checkpointer:
+        
+        graph_with_mongo = compile_graph_with_checkpointer(mongo_checkpointer)
+
+        query = input("> ")
+
+        result = graph_with_mongo.invoke(
+            {"messages": [{"role": "user", "content": query}]},config)
+
+        print(result)
+        
+main()
